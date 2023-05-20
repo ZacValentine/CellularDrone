23eccdbd4ed8a4f978c274fd81f6cd0143a54be7
ZacValentine <112518832+ZacValentine@users.noreply.github.com>
2023-05-20 09:19:25 -0900

    Create keyboardClient.py

---
+++ keyboardClient.py
@@ -0,0 +1,63 @@
+import cv2, imutils, socket
+import numpy as np
+import time
+import base64
+import RPi.GPIO as gpio
+import time
+
+gpio.setmode(gpio.BCM)
+gpio.setwarnings(False)
+
+class motor():
+    def __init__(self, Ena, In1, In2):
+        self.Ena = Ena
+        self.In1 = In1
+        self.In2 = In2
+        gpio.setup(self.Ena, gpio.OUT)
+        gpio.setup(self.In1, gpio.OUT)
+        gpio.setup(self.In2, gpio.OUT)
+        self.pwm = gpio.PWM(self.Ena, 100)
+        self.pwm.start(0)
+    
+    def forward(self, speed = 100):
+        gpio.output(self.In1, gpio.HIGH)
+        gpio.output(self.In2, gpio.LOW)
+        self.pwm.ChangeDutyCycle(speed)
+    def backward(self, speed = 100):
+        gpio.output(self.In1, gpio.LOW)
+        gpio.output(self.In2, gpio.HIGH)
+        self.pwm.ChangeDutyCycle(speed)
+    def stop(self):
+        self.pwm.ChangeDutyCycle(0)
+rightMotor = motor(25, 23, 24)
+leftMotor = motor(27, 17, 22)
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('100.80.57.27', 5000))
+print("connection")
+while True:
+    msg = s.recv(16)
+    data = msg.decode("utf-8")
+    print(data)
+    
+    if(data == "f"):
+        leftMotor.forward(100)
+        rightMotor.forward(100)
+    elif(data == "b"):
+        leftMotor.backward(100)
+        rightMotor.backward(100)
+    elif(data == "r"):
+        leftMotor.forward(100)
+        rightMotor.stop()
+    elif(data == "l"):
+        rightMotor.foward(100)
+        leftMotor.stop()
+    elif(data == "s"):
+        leftMotor.stop()
+        rightMotor.stop()
+    else:
+        leftMotor.stop()
+        rightMotor.stop()
+    

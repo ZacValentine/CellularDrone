137b62e079993888f6770d463363dc39df62722d
ZacValentine <112518832+ZacValentine@users.noreply.github.com>
2023-05-30 19:50:43 -0400

    Update compressedVideoClient.py

--- compressedVideoClient.py
+++ compressedVideoClient.py
@@ -3,12 +3,12 @@ import imutils
 import socket
 import numpy as np
 import base64
+import zlib
 
 BUFF_SIZE = 65536
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
-host_name = socket.gethostname()
-host_ip = '100.88.162.36'
+host_ip = '100.80.57.27'
 port = 5000
 message = b'hello'
 
@@ -16,8 +16,10 @@ client_socket.sendto(message, (host_ip, port))
 
 while True:
     packet, _ = client_socket.recvfrom(BUFF_SIZE)
-    data = base64.b64decode(packet)
-    npdata = np.frombuffer(data, dtype=np.uint8)
+    decoded_packet = base64.b64decode(packet)
+    decompressed_packet = zlib.decompress(decoded_packet)
+
+    npdata = np.frombuffer(decompressed_packet, dtype=np.uint8)
     frame = cv2.imdecode(npdata, cv2.IMREAD_COLOR)
 
     # Modify frame here (e.g., image enhancement)

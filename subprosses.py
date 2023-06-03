1b2fd0994d2994e7384e6020b28ec9459783441b
ZacValentine <112518832+ZacValentine@users.noreply.github.com>
2023-06-03 14:34:53 -0400

    Update subprosses.py

--- subprosses.py
+++ subprosses.py
@@ -1,20 +1,19 @@
-import threading
 import subprocess
-
+import threading
 
 def run_script(script_name):
-    subprocess.Popen([python_path, script_name], cwd='.')
+    subprocess.call([python_path, script_name])
 
 python_path = r'C:\Users\zacv2\PycharmProjects\CellularDrone\venv\Scripts\python.exe'
 script1 = r'C:/Users/zacv2/PycharmProjects/CellularDrone/compressedVideoClient.py'
 script2 = r'C:/Users/zacv2/PycharmProjects/CellularDrone/keyboardServer.py'
 
-# Start script 1 in a separate thread
+# Start keyboard server in a separate thread
 thread1 = threading.Thread(target=run_script, args=(script1,))
 thread1.start()
 
-# Start script 2 in the main thread
+# Run compressed video client script in the main thread
 run_script(script2)
 
-# Wait for thread 1 to complete
+# Wait for thread1 to complete
 thread1.join()

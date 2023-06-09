f2b17822745cce6af18ed6fd80e52d50745c5caa
ZacValentine <112518832+ZacValentine@users.noreply.github.com>
2023-06-08 21:07:27 -0400

    Update videoKeyboardServer.py

--- videoKeyboardServer.py
+++ videoKeyboardServer.py
@@ -4,7 +4,11 @@ import socket
 import base64
 import zlib
 
-BUFF_SIZE = 65536
+
+
+#BUFF_SIZE = 65536
+BUFF_SIZE = 35536
+SML_BUFF_SIZE = 100
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
 host_ip = '100.80.57.27'
@@ -17,8 +21,8 @@ vid = cv2.VideoCapture(0)
 fps, st, frames_to_count, cnt = (0, 0, 20, 0)
 
 while True:
-    # get clieent addr from msg
-    msg, client_addr = server_socket.recvfrom(BUFF_SIZE)
+    # get client addr from msg
+    msg, client_addr = server_socket.recvfrom(SML_BUFF_SIZE)
     print('got connection from', client_addr)
 
     WIDTH = 400
@@ -35,10 +39,9 @@ while True:
         # Send Frame
         server_socket.sendto(encoded_frame, client_addr)
         # recv keys
-        msg, client_addr = server_socket.recvfrom(BUFF_SIZE)
+        msg, client_addr = server_socket.recvfrom(SML_BUFF_SIZE)
         # print keys
         print(msg.decode('utf-8'))
-
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             server_socket.close()

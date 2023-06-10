0dca8965045fea6db23d2cb8b78999163da39d9d
ZacValentine <112518832+ZacValentine@users.noreply.github.com>
2023-06-10 12:02:52 -0900

    Update videoKeyboardServer.py

--- videoKeyboardServer.py
+++ videoKeyboardServer.py
@@ -3,15 +3,17 @@ import imutils
 import socket
 import base64
 import zlib
+from Motor import Motor
 
 
 
-#BUFF_SIZE = 65536
-BUFF_SIZE = 35536
-SML_BUFF_SIZE = 100
+BUFF_SIZE = 65536
+#BUFF_SIZE = 35536
+#SML_BUFF_SIZE = 100
+SML_BUFF_SIZE = 1000
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
-host_ip = '100.80.57.27'
+host_ip = '100.110.162.27'
 port = 5000
 socket_address = (host_ip, port)
 server_socket.bind(socket_address)
@@ -19,6 +21,10 @@ print('listening at:', socket_address)
 
 vid = cv2.VideoCapture(0)
 fps, st, frames_to_count, cnt = (0, 0, 20, 0)
+
+
+rightMotor = Motor(25, 23, 24)
+leftMotor = Motor(27, 17, 22)
 
 while True:
     # get client addr from msg
@@ -41,7 +47,25 @@ while True:
         # recv keys
         msg, client_addr = server_socket.recvfrom(SML_BUFF_SIZE)
         # print keys
-        print(msg.decode('utf-8'))
+        msg = msg.decode('utf-8')
+        print(msg)
+        # move
+        if len(msg) <= 0:
+            leftMotor.stop()
+            rightMotor.stop()
+        elif msg[0] == 'w':
+            leftMotor.forward()
+            rightMotor.forward()
+        elif msg[0] == 's':
+            leftMotor.backward()
+            rightMotor.backward()
+        elif msg[0] == 'a':
+            leftMotor.stop()
+            rightMotor.forward()
+        elif msg[0] == 'd':
+            leftMotor.forward()
+            rightMotor.stop()
+            
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             server_socket.close()

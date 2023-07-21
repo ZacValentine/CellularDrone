b38fb5b666d3bf914c5ccc277f618d10ee3c7a7b
ZacValentine <112518832+ZacValentine@users.noreply.github.com>
2023-07-21 15:26:40 -0400

    Update variableFpsServer.py

--- variableFpsServer.py
+++ variableFpsServer.py
@@ -1,3 +1,6 @@
+
+# ZMQ - LOW LATENCY - SUPPOSED CHANGE FPS WHEN UPLOAD SPEED IS HIGHER THAN SPECIFIED UPLOAD SPEED - LATENCY NOT SUPPOSED TO INCREASE WHEN UPLOAD IS LOWER THAN MBPS
+
 import zmq
 import cv2
 import imutils
@@ -23,12 +26,15 @@ start_time = time.time()
 total_data_sent = 0
 elapsed_frames = 0
 
-# Set the target frame rate (adjust as needed)
-target_frame_rate = 15
+# Set the initial target frame rate (adjust as needed)
+target_frame_rate = 30
 
 # Initialize buffer to store frames
 buffer_size = 5  # Number of frames to store in the buffer
 frame_buffer = []
+
+# Initialize timer for frame rate control
+last_frame_time = time.time()
 
 while True:
     # Get frame
@@ -53,16 +59,19 @@ while True:
         elapsed_frames = 0
 
         # Calculate the desired frame rate to achieve the target bandwidth
-        #target_data_rate = target_frame_rate * 1920 * 1080 * 3 * 8 / 1000000  # Target data rate in Mbps
-        target_data_rate = 4
+        target_data_rate = 4  # Set the desired data rate in Mbps (adjust as needed)
         print("[SERVER] Target data rate:", target_data_rate)
-        if data_sent_per_second > target_data_rate:
-            target_frame_rate -= 1
-        else:
-            target_frame_rate += 1
 
-        # Limit the target frame rate to a minimum of 1 and a maximum of 30
-        target_frame_rate = max(1, min(target_frame_rate, 30))
+        # Calculate the actual frame rate based on elapsed time
+        actual_frame_rate = elapsed_frames / elapsed_time
+
+        # Use a proportional control algorithm to adjust the target frame rate
+        adjustment_factor = 0.1  # Set the proportional control factor (adjust as needed)
+        error = data_sent_per_second - target_data_rate
+        target_frame_rate = max(1, target_frame_rate + int(error * adjustment_factor))
+
+        # Limit the target frame rate to a maximum of 30
+        target_frame_rate = min(target_frame_rate, 30)
 
     # Add the frame to the buffer
     frame_buffer.append(encoded_data)
@@ -71,8 +80,12 @@ while True:
     if len(frame_buffer) > buffer_size:
         frame_buffer.pop(0)
 
-    # Send the most recent frame from the buffer
-    if frame_buffer:
+    # Calculate the actual frame display rate based on elapsed time
+    actual_frame_rate = elapsed_frames / elapsed_time
+
+    # Send the most recent frame from the buffer if it's time for the next frame
+    if frame_buffer and actual_frame_rate >= target_frame_rate:
+        last_frame_time = time.time()
         latest_frame = frame_buffer.pop()
         socket.send(latest_frame)
         total_data_sent += len(latest_frame)
